59e545a5497dab063351af9d69914964bbb97884
Amol M <117634099+Amol5766@users.noreply.github.com>
2024-07-13 20:38:22 +0530

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+// config-overrides.js
+module.exports = function override(config, env) {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false
+      }
+    });
+  
+    config.ignoreWarnings = config.ignoreWarnings || [];
+    config.ignoreWarnings.push({
+      module: /@mediapipe\/tasks-vision/
+    });
+  
+    return config;
+  };
+  
